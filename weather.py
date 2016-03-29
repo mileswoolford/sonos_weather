1da5ab885836cefccbef6fda70d5922bd3eb02a2
Alex Woolford <Alex.Woolford@i-behavior.com>
2016-03-28 20:15:17 -0600

    Basic temperature API calls

--- weather.py
+++ weather.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
+import pyowm
 import ConfigParser
-
 
 config = ConfigParser.RawConfigParser()
 config.read('sonos_weather.cfg')
 
 api_key = config.get('weather', 'api_key')
+location = "Lafayette, CO, USA"
 
-print api_key
+owm = pyowm.OWM(api_key)
 
+observation = owm.weather_at_place(location)
+weather = observation.get_weather()
 
-
-
+temperature_fahrenheit = weather.get_temperature('fahrenheit')
